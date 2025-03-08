a16d39db7978ccfd607289c35e02e07db64a696d
ywyh <yassienwyh0@gmail.com>
2025-03-08 15:01:46 +0200

    s

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -4,6 +4,9 @@
     users.${user} = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" "docker" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE96xNPyTlTWVtgId/gy3zZ1wfXyz3BexjhyRlPzXk1c yassienwyh0@gmail.com"
+      ];
     };
   };
 }
